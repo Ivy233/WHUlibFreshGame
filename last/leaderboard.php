83f78a29b66b9170b8b0ccedf4e0cb50e3a98050
Ivy233 <952254420@qq.com>
2018-10-10 14:51:57 +0800

    lastone？

--- last/leaderboard.php
+++ last/leaderboard.php
@@ -15,23 +15,11 @@ if(!empty($_POST['stunum']))
     $res_nearby=array();
     $user['challenge_best']=intval($user['challenge_best']);
     $user['challenge_first']=intval($user['challenge_first']);
-    $top_100=$db->getAll("select * from user_game where challenge_best>0 
-                          order by challenge_best desc, challenge_time asc, challenge_first asc limit 100");
-    $myrank=$db->getRow("select count(*) from user_game 
-                         where challenge_best>'".$user['challenge_best']."' 
-                         or (challenge_best='".$user['challenge_best']."' and challenge_time<'".$user['challenge_time']."')
-                         or (challenge_best=".$user['challenge_best']." and challenge_time=".$user['challenge_time']." and challenge_first<".$user['challenge_first'].")");
+    $top_100=$db->getAll("select * from user_game where challenge_best>0 order by challenge_best desc, challenge_first asc limit 100");
+    $myrank=$db->getRow("select count(*) from user_game where challenge_best>'".$user['challenge_best']."' or (challenge_best='".$user['challenge_best']."' and challenge_first<'".$user['challenge_first']."')");
     $myrank=$myrank['count(*)']+1;
-    $prev2=$db->getAll("select * from user_game where (
-                        challenge_best>'".$user['challenge_best']."' 
-                        or ( challenge_best='".$user['challenge_best']."' and challenge_time<'".$user['challenge_time']."' )
-                        or (challenge_best=".$user['challenge_best']." and challenge_time=".$user['challenge_time']." and challenge_first<".$user['challenge_first'].")
-                        ) order by challenge_best asc, challenge_time desc limit 1");
-    $next2=$db->getAll("select * from user_game where (
-                        challenge_best<'".$user['challenge_best']."' 
-                        or ( challenge_best='".$user['challenge_best']."' and challenge_time>'".$user['challenge_time']."' )
-                        or (challenge_best=".$user['challenge_best']." and challenge_time=".$user['challenge_time']." and challenge_first>".$user['challenge_first'].")
-                        ) order by challenge_best desc, challenge_time asc limit 1");
+    $prev2=$db->getAll("select * from user_game where (challenge_best>'".$user['challenge_best']."' or ( challenge_best='".$user['challenge_best']."' and challenge_first<'".$user['challenge_first']."' )) order by challenge_best asc, challenge_first desc limit 1");
+    $next2=$db->getAll("select * from user_game where (challenge_best<'".$user['challenge_best']."' or ( challenge_best='".$user['challenge_best']."' and challenge_first>'".$user['challenge_first']."' )) order by challenge_best desc, challenge_first asc limit 1");
     foreach($top_100 as $key=>$val)
     {
         array_push($res_top_100,array(
